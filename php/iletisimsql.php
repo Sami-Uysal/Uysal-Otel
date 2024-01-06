45a5a23c07a0455aba0323772c8afff815a8e782
Sami Uysal <samiuysal.business@gmail.com>
2024-01-06 22:26:43 +0300

    PhpMailer added

--- php/iletisimsql.php
+++ php/iletisimsql.php
@@ -1,18 +1,43 @@
 <?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    // Gerekli dosyaları dahil edin
+    require 'PHPMailer/src/Exception.php';
+    require 'PHPMailer/src/PHPMailer.php';
+    require 'PHPMailer/src/SMTP.php';
+    require 'PHPMailer/language/phpmailer.lang-tr.php';
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $ad = $_POST['ad'];
         $email = $_POST['email'];
         $mesaj = $_POST['mesaj'];
 
-        $alici_email = "samiuysal.business@gmail.com";
-        $konu = "İletişim Formundan Yeni Mesaj";
-        $icerik = "Ad: $ad\n E-posta: $email\n Mesaj: $mesaj";
-        $basliklar = "From: $email\r\nReply-To: $email\r\n";
+    
+        $mail = new PHPMailer(true);
 
-        if (mail($alici_email, $konu, $icerik, $basliklar)) {
-            echo "E-posta başarıyla gönderildi, teşekkür ederiz!";
-        } else {
-            echo "E-posta gönderilirken bir hata oluştu.";
+        try {
+            // SMTP ayarları
+            $mail->isSMTP();
+            $mail->Host       = 'smtp.gmail.com';
+            $mail->SMTPAuth   = true;
+            $mail->Username   = 'uysalotel.iletisim@gmail.com';
+            $mail->Password   = 'kfvx poqh yudp jitl';
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+            $mail->Port       = 587;
+
+            $mail->setFrom('uysalotel.iletisim@gmail.com', 'Uysal Iletisim');
+
+            $mail->addAddress('uysalotel.iletisim@gmail.com', 'Iletisim');
+
+            $mail->isHTML(true);
+            $mail->Subject = 'Iletisim Formu';
+            $mail->Body    = "Ad: $ad<br>E-posta: $email<br>Mesaj: $mesaj";
+
+            $mail->send();
+            echo 'E-posta başarıyla gönderildi!';
+        } catch (Exception $e) {
+            echo 'E-posta gönderilirken bir hata oluştu: ', $mail->ErrorInfo;
         }
     }
 ?>
