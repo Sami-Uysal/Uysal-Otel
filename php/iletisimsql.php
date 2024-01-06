c375c37e602116bba93efe45d94827a0ac79ec2b
Sami Uysal <samiuysal.business@gmail.com>
2024-01-06 20:11:19 +0300

    Add mail

--- php/iletisimsql.php
+++ php/iletisimsql.php
@@ -4,6 +4,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $mesaj = $_POST['mesaj'];
 
-    
+    $alici_email = "samiuysal.business@gmail.com";
+
+    $konu = "İletişim Formundan Yeni Mesaj";
+
+    $icerik = "Ad: $ad\n E-posta: $email\n Mesaj: $mesaj";
+
+    $basliklar = "From: $email\r\nReply-To: $email\r\n";
+
+    if (mail($alici_email, $konu, $icerik, $basliklar)) {
+        echo "<p>E-posta başarıyla gönderildi, teşekkür ederiz!</p>";
+    } else {
+        echo "<p>E-posta gönderilirken bir hata oluştu.</p>";
+    }
 }
 ?>
