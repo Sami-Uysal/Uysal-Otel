e19dc38b8bfd0ed5ebb17ffdc3f26b8cac66b403
Sami Uysal <samiuysal.business@gmail.com>
2024-01-12 11:42:11 +0300

    Changes have been made.

--- hakkimizda.php
+++ hakkimizda.php
@@ -17,7 +17,7 @@ if (file_exists('txt/icerik.txt')) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" icerik="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Uysal Otel</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@500;700&family=Open+Sans&display=swap" rel="stylesheet">
@@ -37,9 +37,11 @@ include 'php/navbar.php';
   <div  class="container">
     <h2>Hakkımızda</h2>
     <form action="" method="post">
-      <textarea id="editor" name="editor_icerik" rows="27" cols="100" readonly>
-        <?php echo $icerik; ?>
-      </textarea>
+      <div>
+        <textarea id="editor" name="editor_icerik" rows="27" cols="100" readonly>
+          <?php echo $icerik; ?>
+        </textarea>
+      </div>
       <?php if($kullaniciadi === 'admin') echo  '<button type="submit" class="btn btn-primary">Kaydet</button>' ?>
     </form>
   </div>
